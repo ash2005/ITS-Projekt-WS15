5f2e08abe27768d6b34fb5ba837d4eac3924695e
Konstantin Ruhmann <konstantin.ruhmann@googlemail.com>
2015-10-23 07:18:46 +0200

    formatierung

--- src/Week1/PlainRSA.java
+++ src/Week1/PlainRSA.java
@@ -14,71 +14,58 @@ import org.bouncycastle.util.encoders.Hex;
 public class PlainRSA {
 
 	public static void main(String[] args) {
-		//data
+		// data
 		String input = "Test";
 		byte[] data = input.getBytes();
 		byte[] encrData = null;
-        byte[] decrData = null;
-        
-		System.out.println("Text: " + input + " Länge: " + data.length);
-		
-		
-		//Create Keys
-		RSAKeyPairGenerator keygen = new RSAKeyPairGenerator();
-		keygen.init(new RSAKeyGenerationParameters
-			    		(
-			            new BigInteger("10001", 16),//publicExponent - 65537
-			            new SecureRandom(),//prng
-			            2048,//strength
-			            80//certainty
-			            )
-			    );
-		
-		AsymmetricCipherKeyPair keypair = keygen.generateKeyPair();
-		
-		
-		// Encryption
-		AsymmetricBlockCipher   engEn = new RSAEngine();
-        engEn.init(true, keypair.getPublic());        
-        
-        //System.out.println(engEn.getInputBlockSize());
-        //System.out.println(engEn.getOutputBlockSize());
-        
-        try {
-            encrData = engEn.processBlock(data, 0, data.length);
-        } catch (InvalidCipherTextException error) {
-            error.printStackTrace();
-        }
-		
-        // Decryption
-        AsymmetricBlockCipher   engDe = new RSAEngine();        
-        engDe.init(false,  keypair.getPrivate());
-        
-        try {
-            decrData = engDe.processBlock(encrData, 0, encrData.length);
-        } catch (InvalidCipherTextException error) {
-            error.printStackTrace();
-        }
+		byte[] decrData = null;
 
-        
-        String text0 = Hex.toHexString(data);
-        String text1 = Hex.toHexString(encrData);
-        String text2 = Hex.toHexString(decrData);
-        
-        System.out.println(
-        		"Input: " + text0 + "\n" + 
-                "Encr:  " + text1 + "\n" + 
-                "Decr: " + text2
-                );
-        
-        
-        if (text0.equals(text2)) {
-            System.out.println("Success");
-        } else {
-            System.out.println("Fail");
-        }
-        
-		
+		System.out.println("Text: " + input + " Länge: " + data.length);
+
+		// Create Keys
+		RSAKeyPairGenerator keygen = new RSAKeyPairGenerator();
+		keygen.init(new RSAKeyGenerationParameters(new BigInteger("10001", 16), // publicExponent
+				new SecureRandom(), // prng
+				2048, // strength
+				80// certainty
+		));
+
+		AsymmetricCipherKeyPair keypair = keygen.generateKeyPair();
+
+		// Encryption
+		AsymmetricBlockCipher engEn = new RSAEngine();
+		engEn.init(true, keypair.getPublic());
+
+		// System.out.println(engEn.getInputBlockSize());
+		// System.out.println(engEn.getOutputBlockSize());
+
+		try {
+			encrData = engEn.processBlock(data, 0, data.length);
+		} catch (InvalidCipherTextException error) {
+			error.printStackTrace();
+		}
+
+		// Decryption
+		AsymmetricBlockCipher engDe = new RSAEngine();
+		engDe.init(false, keypair.getPrivate());
+
+		try {
+			decrData = engDe.processBlock(encrData, 0, encrData.length);
+		} catch (InvalidCipherTextException error) {
+			error.printStackTrace();
+		}
+
+		String text0 = Hex.toHexString(data);
+		String text1 = Hex.toHexString(encrData);
+		String text2 = Hex.toHexString(decrData);
+
+		System.out.println("Input: " + text0 + "\n" + "Encr:  " + text1 + "\n" + "Decr: " + text2);
+
+		if (text0.equals(text2)) {
+			System.out.println("Success");
+		} else {
+			System.out.println("Fail");
+		}
 
 	}
 
